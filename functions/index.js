866efa0eb07948da404e0562c42d3753d64c82ad
Austin Theriot <austinmtheriot@gmail.com>
2020-07-28 20:28:17 -0500

    rough draft complete

--- functions/index.js
+++ functions/index.js
@@ -1,64 +1,86 @@
+const firebase = require('firebase');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+require('firebase/firestore');
+
 const nodemailer = require('nodemailer');
 const cors = require('cors')({ origin: true });
-const config = require('./config');
 
 admin.initializeApp();
+const config = require('./config');
+firebase.initializeApp(config.firebase);
+const db = admin.firestore();
 
 exports.sendEmail = functions.https.onRequest((req, res) => {
-	if (req.body.key === config.key) {
-		return cors(req, res, () => {
-			// getting dest email by query string
-			// const dest = req.query.dest;
+	//first retrieve available API keys from Firebase database
+	return db
+		.collection('keys')
+		.doc(req.body._private.key)
+		.get()
+		.then((doc) => {
+			let database = doc.data();
+			if (doc.exists) {
+				return cors(req, res, () => {
+					//generate message based on request body
+					//filter out private API data
+					let messageList = [...Object.keys(req.body)]
+						.filter((key) => !key.match(/_private/gi))
+						.map((key) => {
+							console.log(key, req.body[key]);
+							return `<p>${key}: ${req.body[key]}</p>`;
+						})
+						.join(' ');
 
-			//generate message based on request body
-			//DO NOT include meessage if the message is the key or website source
-			let messageList = [...Object.keys(req.body)]
-				.filter((key) => !key.match(/key/gi) && key !== 'source')
-				.map((key) => {
-					console.log(key, req.body[key]);
-					return `<p>${key}: ${req.body[key]}</p>`;
-				})
-				.join(' ');
+					let emailBody = `
+						Hi ${database.name || 'there'},
+						<br/>
+						<br/>
+						You have received a new form submission for ${
+							database.website || 'your website'
+						}:
+						<br/>
+						${messageList}
+						<br/>
+						<br/>
+						<br/>
+						Notice: Please do not reply directly to this email.
+        `;
 
-			let emailBody = `
-        <p>You have received a new form submission for your website, ${req.body.Source}:</p>
-        ${messageList}
-        <br/>
-        <br/>
-        <br/>
-        <p>Notice: Please do not reply directly to this email.</p>
-      `;
+					let transporter = nodemailer.createTransport({
+						host: 'smtp.gmail.com',
+						auth: {
+							type: 'login', // default
+							user: config.email.username,
+							pass: config.email.password,
+						},
+					});
 
-			let transporter = nodemailer.createTransport({
-				host: 'smtp.gmail.com',
-				auth: {
-					type: 'login', // default
-					user: config.username,
-					pass: config.password,
-				},
-			});
+					const mailOptions = {
+						from: config.email.fromEmail, // Something like: Jane Doe <janedoe@gmail.com>
+						to: database.destination,
+						subject: `New Form Submission (${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()})`, // email subject
+						html: emailBody, // email content in HTML
+					};
 
-			const mailOptions = {
-				from: config.fromEmail, // Something like: Jane Doe <janedoe@gmail.com>
-				to: config.toEmail,
-				subject: `New Form Submission (${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()})`, // email subject
-				html: emailBody, // email content in HTML
-			};
-
-			// returning result
-			transporter.sendMail(mailOptions, (error, data) => {
-				if (error) {
-					return res.send(error.toString());
-				}
-				data = JSON.stringify(data);
-				return res.status(200).send({ message: `Sent!`, email: emailBody });
-			});
+					// returning result
+					transporter.sendMail(mailOptions, (error, data) => {
+						if (error) {
+							return res.send(error.toString());
+						}
+						data = JSON.stringify(data);
+						return res.status(200).send({
+							message: `Your form was successfully submitted!`,
+							redirect: database.redirect,
+						});
+					});
+				});
+			} else {
+				return res.status(400).send({
+					error: `Unauthorized Request. Please use the correct key to send an email through this service.`,
+				});
+			}
+		})
+		.catch((error) => {
+			return res.status(500).send({ message: error });
 		});
-	} else {
-		return res.status(400).send({
-			error: `Unauthorized Request. Please use the correct key to send an email through this service.`,
-		});
-	}
 });
