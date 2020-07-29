3a50b8521e63a8ed6878ef96a80db7bc5c9fb524
Austin Theriot <austinmtheriot@gmail.com>
2020-07-28 22:59:46 -0500

    disabled linter on promise returns

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable consistent-return */
+/* eslint-disable promise/always-return */
 const firebase = require('firebase');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
@@ -25,17 +27,16 @@ app.post('/', (req, res) => {
 		.then((doc) => {
 			let database = doc.data();
 			if (doc.exists) {
-				return cors(req, res, () => {
-					//generate message based on request body
-					//filter out private API data
-					let messageList = [...Object.keys(req.body)]
-						.filter((key) => !key.match(/_private/gi))
-						.map((key) => {
-							return `<p>${key}: ${req.body[key]}</p>`;
-						})
-						.join(' ');
+				//generate message based on request body
+				//filter out private API data
+				let messageList = [...Object.keys(req.body)]
+					.filter((key) => !key.match(/_private/gi))
+					.map((key) => {
+						return `<p>${key}: ${req.body[key]}</p>`;
+					})
+					.join(' ');
 
-					let emailBody = `
+				let emailBody = `
 				Hi ${database.name || 'there'},
 				<br/>
 				<br/>
@@ -50,58 +51,45 @@ app.post('/', (req, res) => {
 				Notice: Please do not reply directly to this email.
 		`;
 
-					let transporter = nodemailer.createTransport({
-						host: 'smtp.gmail.com',
-						auth: {
-							type: 'login', // default
-							user: config.email.username,
-							pass: config.email.password,
-						},
-					});
+				let transporter = nodemailer.createTransport({
+					host: 'smtp.gmail.com',
+					auth: {
+						type: 'login', // default
+						user: config.email.username,
+						pass: config.email.password,
+					},
+				});
 
-					const mailOptions = {
-						from: config.email.fromEmail, // Something like: Jane Doe <janedoe@gmail.com>
-						to: database.destination,
-						subject: `New Form Submission (${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()})`, // email subject
-						html: emailBody, // email content in HTML
-					};
+				const mailOptions = {
+					from: config.email.fromEmail, // Something like: Jane Doe <janedoe@gmail.com>
+					to: database.destination,
+					subject: `New Form Submission (${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()})`, // email subject
+					html: emailBody, // email content in HTML
+				};
 
-					// returning result
-					transporter.sendMail(mailOptions, (error, data) => {
-						if (error) {
-							return res
-								.set('Access-Control-Allow-Origin', '*')
-								.set('Access-Control-Allow-Headers', 'Content-Type')
-								.status(500)
-								.send(error.toString());
-						}
-						data = JSON.stringify(data);
-						return res
-							.set('Access-Control-Allow-Origin', '*')
-							.set('Access-Control-Allow-Headers', 'Content-Type')
-							.status(200)
-							.send({
-								message: `Your form was successfully submitted!`,
-								redirect: database.redirect,
-							});
+				// returning result
+				transporter.sendMail(mailOptions, (error, data) => {
+					if (error) {
+						//failed to send mail
+						return res.status(500).send(error.toString());
+					}
+					data = JSON.stringify(data);
+					//succeeded at sending mail
+					return res.status(200).send({
+						message: `Your form was successfully submitted!`,
+						redirect: database.redirect,
 					});
 				});
 			} else {
-				return res
-					.set('Access-Control-Allow-Origin', '*')
-					.set('Access-Control-Allow-Headers', 'Content-Type')
-					.status(400)
-					.send({
-						error: `Unauthorized Request. Please use the correct key to send an email through this service.`,
-					});
+				//bad request (wrong key)
+				return res.status(400).send({
+					error: `Unauthorized Request. Please use the correct key to send an email through this service.`,
+				});
 			}
 		})
 		.catch((error) => {
-			return res
-				.set('Access-Control-Allow-Origin', '*')
-				.set('Access-Control-Allow-Headers', 'Content-Type')
-				.status(500)
-				.send({ message: error });
+			//else, catch any other errors
+			return res.status(500).send({ message: error });
 		});
 });
 
